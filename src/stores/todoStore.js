5979820aa1057cd45f45bc64be0c49a1d8a2f4ab
unknown <john.tamad147@gmail.com>
2022-04-12 09:34:20 +0800

    fix firebase update todos

--- src/stores/todoStore.js
+++ src/stores/todoStore.js
@@ -12,35 +12,11 @@ export const useTodoStore = defineStore("useTodoStore", {
         firebase
           .firestore()
           .collection("todos")
-          .orderBy("createdAt", "desc")
+          .orderBy("createdAt")
           .onSnapshot((res) => {
             let querySnapshot = res.docChanges();
             querySnapshot.forEach((todo) => {
-              console.log(todo.type);
               if (todo.type == "added" && todo.doc.data().createdAt != null) {
-                let date = todo.doc.data().createdAt.toDate();
-
-                var today = date
-                  .toLocaleDateString("en-GB", {
-                    day: "numeric",
-                    month: "short",
-                    year: "numeric",
-                  })
-                  .split(" ")
-                  .join("-");
-
-                this.todos.push({
-                  createdAt: today,
-                  text: todo.doc.data().text,
-                  done: todo.doc.data().done,
-                  id: todo.doc.id,
-                });
-              }
-
-              if (
-                todo.type === "modified" &&
-                this.todos.shift.id != todo.doc.data().id
-              ) {
                 let dateMod = todo.doc.data().createdAt.toDate();
 
                 var todayMod = dateMod
@@ -72,29 +48,30 @@ export const useTodoStore = defineStore("useTodoStore", {
           .collection("todos")
           .add(payload)
           .then(() => {
-            // let dateMod = payload.createdAt.toDate();
-
-            // var todayMod = dateMod
-            //   .toLocaleDateString("en-GB", {
-            //     day: "numeric",
-            //     month: "short",
-            //     year: "numeric",
-            //   })
-            //   .split(" ")
-            //   .join("-");
-            //payload.createdAt = todayMod;
-            // console.log(payload.createdAt);
-            // this.todos.unshift(payload);
             resolve();
           });
       });
     },
     updateTodo(payload) {
-      firebase.firestore().collection("todos").doc(payload.id).update({
-        text: payload.text,
-        done: payload.done,
-        updatedAt: firebase.firestore.FieldValue.serverTimestamp(),
-      });
+      firebase
+        .firestore()
+        .collection("todos")
+        .doc(payload.id)
+        .update({
+          text: payload.text,
+          done: payload.done,
+          updatedAt: firebase.firestore.FieldValue.serverTimestamp(),
+        })
+        .then(() => {
+          const index = this.todos.findIndex((x) => x.id === payload.id);
+          console.log(index);
+          console.log("Debug here");
+          if (index < 0) {
+            throw new Error(`Can't find todo item [${payload.id}]`);
+          }
+
+          this.todos.splice(index, 1, payload);
+        });
     },
   },
 });
